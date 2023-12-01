93fc2f79cedc985f4a3be187694e2eb63319a96d
ИСП-31 <-31@LAB17-04>
2023-12-01 15:47:39 +0300

    Добавлены комментарии для структуры

--- Prakt9/Films.cs
+++ Prakt9/Films.cs
@@ -1,11 +1,11 @@
 ﻿namespace Prakt9
 {
-    struct Films
+    struct Films // Фильмотека
     {
-        public string Name { get; set; }
-        public string Genre { get; set; }
-        public int Year { get; set; }
-        public int Length { get; set; }
+        public string Name { get; set; } // Название фильма
+        public string Genre { get; set; } // Жанр фильма
+        public int Year { get; set; } // Год выпуска
+        public int Length { get; set; } // Продолжительность просмотра
 
         public Films(string name, string genre, int year, int length)
         {
